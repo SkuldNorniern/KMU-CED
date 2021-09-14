0e1fdf70e4c03a6e685cfa1b57bc6e28eecf9d1b
Skuld Norniern <43695854+SkuldNorniern@users.noreply.github.com>
2021-09-14 11:50:33 +0900

    [FIX] fixed error for example 3
    /week 2
        /04_example_3.ino

--- week 2/04_example_3.ino
+++ week 2/04_example_3.ino
@@ -1,22 +1,22 @@
 #define PIN_LED 13
-unsigned int count, toggle;
+
+unsigned int count;
+bool toggle;
 void setup() {
-  pinMode(PIN_LED, OUTPUT);
   Serial.begin(115200);
-  while (!Serial){
+
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, toggle);
+
+  while (!Serial)
     ;
-  }
-  Serial.println("Hello World!");
-  count=toggle=0;
-  digitalWrite(PIN_LED,toggle);
 }
 
 void loop() {
   Serial.println(++count);
   toggle = toggle_state(toggle);
-  digitalWrite(PIN_LED,toggle);
+  digitalWrite(PIN_LED, toggle);
   delay(1000);
 }
-int toggle_state(int toggle){
-  return toggle;
-}
+
+bool toggle_state(bool toggle) { return !toggle; }
